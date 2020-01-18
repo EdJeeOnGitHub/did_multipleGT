4e96a07ba778f03eaaf4cbe40e28c315a710fc56
Ed Jee <edjee96@gmail.com>
2020-01-18 16:09:11 -0500

    added wagepan for unit tests and replication

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Panel dataset of wages
+#'
+#' A dataset containing wage data from F. Vella and M. Verbeek (1998).
+#'
+#' Can be installed via Stata using: `ssc install bcuse; bcuse wagepan, clear`.
+#' See http://fmwww.bc.edu/ec-p/data/wooldridge/datasets.list.html for more
+#' details.
+#'
+#' Variables used for unit tests and replication of de Chaisemartin, Clément
+#'  and d'Haultfoeuille, Xavier (October 30, 2019) http://dx.doi.org/10.2139/ssrn.3148607.
+#'
+#'
+#' @format A data frame with 4360 rows and 27 variables.
+#'
+#' @source \url{http://fmwww.bc.edu/ec-p/data/wooldridge/datasets.list.html}
+"wagepan"
