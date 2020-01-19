79d6d1737508f1d0ad915c1d7d56f22de559ede2
Ed Jee <edjee96@gmail.com>
2020-01-18 21:50:36 -0500

    default arguments added and cleaned up some base R code

--- R/did_multipleGT.R
+++ R/did_multipleGT.R
@@ -31,24 +31,23 @@ did_multiplegt_core <- function(data){
 #' @examples
 did_multipleGT_check <- function(data,
                                  variable_list,
-                                 RECAT_treatment,
-                                 THRESHOLD_stable_treatment,
-                                 trends_nonparam,
-                                 trends_lin,
+                                 RECAT_treatment = NULL,
+                                 THRESHOLD_stable_treatment = 0,
+                                 trends_nonparam = NULL,
+                                 trends_lin = NULL,
                                  controls,
-                                 weight,
-                                 placebo,
-                                 dynamic,
-                                 breps,
-                                 cluster,
-                                 covariances){
+                                 weight = NULL,
+                                 placebo = 0,
+                                 dynamic = 0,
+                                 breps = 0,
+                                 cluster = NULL,
+                                 covariances = FALSE){
   # First 10 lines in the stata program drops missing and produces a subsample
   # if `if` defined.
   #
   # We probably don't want to drop data/produce subsamples since this
   # isn't common in R?
-  
-  
+  data <- as.data.frame(data) # Since tibble naming a bit weird
   # Creating the Y, G, T, D variables
   # TODO: Just pass these as args directly instead of a list?
   data$outcome_XX <- data[, variable_list[1]]
@@ -57,28 +56,27 @@ did_multipleGT_check <- function(data,
   data$treatment_XX <- data[, variable_list[4]]
   
   # When the weight option is specified, the data has to be at the (g,t) level.
-  n_by_group <- aggregate(outcome_XX ~ group_XX + time_XX, data = data, FUN = length)
-  
-  if (max(n_by_group) > 1 & !is.null(weight)) {
+  n_by_group <- aggregate(formula = outcome_XX ~ group_XX + time_XX, data = data, FUN = length)
+  if (max(n_by_group$outcome_XX) > 1 & !is.null(weight)) {
     stop("You have specified the weight option but your data is not aggregated 
           at the group*time level, the command cannot run, aggregate your data
           at the group*time level before running it.")
   }
   # Counting time periods and checking at least two time periods
-  if (unique(na.omit(data$time_XX)) < 2) {
+  if (length(unique(na.omit(data$time_XX))) < 2) {
     stop("There are less than two time periods in the data, the command cannot run.")
   }
   
   # Creating a discretized treatment even if recat_treatment option not specified
   if (!is.null(RECAT_treatment)) {
-    data$D_cat_XX <- data$RECAT_treatment
+    data$D_cat_XX <- data[, "RECAT_treatment"]
   } else {
-    data$D_cat_XX <- treatment_XX
+    data$D_cat_XX <- data[, "treatment_XX"]
   }
   #  Creating groups of recategorized treatment, to ensure we have an ordered 
   # treatment with interval of 1 between consecutive values
-  d_cat_group_XX <- with(data, ave(rep(1, nrow(data)), D_cat_XX, FUN = seq_along))
-  if (unique(d_cat_group_XX) == 1) {
+  data$d_cat_group_XX <- with(data, ave(rep(1, nrow(data)), D_cat_XX, FUN = seq_along))
+  if (length(unique(data$d_cat_group_XX)) == 1) {
     stop("Either the treatment variable or the recategorized treatment in the 
          recat_treatment option takes only one value, the command cannot run.")
   }
